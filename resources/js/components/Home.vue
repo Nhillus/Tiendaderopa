a77aaa69ba8d42f4b97949796f367a1b392c0e2b
Nhillus <xabier762@gmail.com>
2021-01-22 23:20:00 -0500

    Agregado el login y registro en sus vistas vue, funcionando medianamente con un estilo parecido a dein mañana subire el resto del diseño

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola page</title>
+</head>
+<body>
+    <div class="text-2xl text-gray-800">
+        Home page
+    </div>
+    
+</body>
+</html>
+    
+</template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
