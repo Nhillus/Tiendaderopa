c5945f13d2bfa1f892a46dc4ebc11ad655325fd7
Nhillus <xabier762@gmail.com>
2021-03-07 15:08:56 -0500

    Actuaizacion con deployer para permitir hacer deployments desde el github prueba numero 1

---
+++ deployer_task.php
@@ -0,0 +1,6 @@
+<?php
+namespace Deployer;
+desc('Symlink Laravel public To public_html');
+task('app:symlink_public_html', function () {
+run('ln -s /home/lebuurrf/Lebuya/current/public /home/lebuurrf/public_html');
+});
